8cd5150068ec0feeae403f3e8701d05cb42aa2ad
judaihyun <judaihyun1@gmail.com>
2025-05-03 13:29:56 +0900

    sync

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    headers: async () => {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*"
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-CSRF-Token, X-Requested-With, Authorization"
+                    }
+                ]
+            },
+            {
+                source: "/_next/static/:path*",
+                headers: [
+                    { key: "Cache-Control", value: "no-cache, max-age=0" }
+                ]
+            }
+        ];
+    }
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
